774f826f1482c8af3ca7eef4b1fdd6d3403ab2ff
Xasan Nosirov <xasannosirov094@gmail.com>
2024-07-15 16:09:54 +0500

    update address

--- internal/app/main.go
+++ internal/app/main.go
@@ -4,7 +4,6 @@ import (
 	"archv1/internal/pkg/casbin"
 	"archv1/internal/pkg/config"
 	"archv1/internal/pkg/repo/postgres"
-	"archv1/internal/pkg/repo/redis"
 	"archv1/internal/router"
 	"fmt"
 	"log"
@@ -14,13 +13,11 @@ func main() {
 	cfg := config.NewConfig()
 
 	psql := postgres.NewDB(cfg)
-	cache := redis.NewRedis(cfg)
 	enforcer := casbin.NewEnforcer(cfg)
 
 	engine := router.New(&router.Router{
 		Conf:       cfg,
 		PostgresDB: psql,
-		RedisCache: cache,
 		Enforcer:   enforcer,
 	})
 

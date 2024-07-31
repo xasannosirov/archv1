adba097021c7578413c9b301407bcd1702d55600
Xasan Nosirov <xasannosirov094@gmail.com>
2024-07-31 18:34:40 +0500

    add chat entity

--- internal/entity/chat.go
+++ internal/entity/chat.go
@@ -74,3 +74,51 @@ type UserChatsResponse struct {
 		Creator  int    `json:"creator"`
 	}
 }
+
+type SendMessageRequest struct {
+	Action   string `json:"action"`
+	Property struct {
+		ChatID   *int   `json:"chat_id"`
+		ChatType string `json:"chat_type"`
+		Message  string `json:"message"`
+		Sender   int    `json:"sender"`
+	} `json:"property"`
+}
+
+type UpdateMessageRequest struct {
+	Action   string `json:"action"`
+	Property struct {
+		ChatID     int    `json:"chat_id"`
+		ChatType   string `json:"chat_type"`
+		MessageID  int    `json:"message_id"`
+		NewMessage string `json:"new_message"`
+	}
+}
+
+type DeleteMessageRequest struct {
+	Action   string `json:"action"`
+	Property struct {
+		ChatID    int    `json:"chat_id"`
+		ChatType  string `json:"chat_type"`
+		MessageID int    `json:"message_id"`
+	}
+}
+
+type ChatMessagesResponse struct {
+	Messages []struct {
+		ChatID   int    `json:"chat_id"`
+		ChatType string `json:"chat_type"`
+		Sender   int    `json:"sender"`
+		Message  string `json:"content"`
+	}
+}
+
+type NotificationsResponse struct {
+	Notifications []struct {
+		ChatID             int    `json:"chat_id"`
+		ChatType           string `json:"chat_type"`
+		LatestSender       int    `json:"latest_sender"`
+		LatestMessage      string `json:"latest_message"`
+		TotalMessagesCount int    `json:"total_messages_count"`
+	}
+}
